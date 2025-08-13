b05d3c4ca95bb3454404e1c7210bb7e28a366905
recme <recolorme@gmail.com>
2025-08-13 02:04:45 -0400

    hit text bugfix

--- battle_actor_button.gd
+++ battle_actor_button.gd
@@ -16,5 +16,10 @@ func _on_data_hp_changed(hp: int, change: int) -> void:
 	add_child(hit_text)
 	hit_text.position = Vector2(size.x * 0.5,-4)
 	
+	var tween = create_tween()
+	tween.tween_property(hit_text, "position:y", hit_text.position.y - 20, 0.5)
+	tween.tween_property(hit_text, "modulate:a", 0.0, 0.5)
+	tween.finished.connect(func(): hit_text.queue_free())
+	
 func _on_data_defeated() -> void:
 	pass
