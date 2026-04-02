6b39f6c4a1a38c8c10d32a03e6af03c236c3e959
Roku'sWing <WaluigiBreaker@gmail.com>
2026-04-02 12:57:53 -0400

    Created basic enemy spawner script. Deleted enemy node so it spawns randomly instead.

--- Overworld/basic_enemy.gd
+++ Overworld/basic_enemy.gd
@@ -11,10 +11,8 @@ func _process(_delta: float) -> void:
 func _on_interact_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("mamaguebo")
-		
+
 		# save the player position before transitioning to battle scene
 		PlayerVariables.xpos = body.position.x
 		PlayerVariables.ypos = body.position.y
-
-		transform.origin = Vector2(randf_range(0, 120), randf_range(0, 120)) # randomize enemy position
 		get_tree().change_scene_to_file.call_deferred("res://Battle/battle.tscn")
